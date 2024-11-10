b2a522640fb325dab59c0d6bca99e1ac19871de7
Taurvi <srimbakusumo@gmail.com>
2024-11-10 10:50:31 -0800

    Fixes prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,12 @@ export default defineNuxtConfig({
 
   compatibilityDate: '2024-11-03',
 
+  nitro: {
+    prerender: {
+      ignore: ['/profile'],
+    },
+  },
+
   eslint: {
     config: {
       stylistic: true,
